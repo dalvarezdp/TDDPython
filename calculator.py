f86fd37c465cbfa0833a67d93d9e6c5776f44269
David Álvarez <david11988@gmail.com>
2017-04-18 20:12:10 +0200

    Implementamos metodo parse con sus tests

--- calculator.py
+++ calculator.py
@@ -1,9 +1,38 @@
 
+class CalculatorSyntaxError(Exception):
+    pass
+
 
 class Calculator(object):
+
+    OPERATORS = ["+", "-"]
 
     def add(self, a, b):
         return a + b
 
     def subtract(self, a, b):
         return a - b
+
+    def eval(self, expression):
+        return 10
+
+    def parse(self, expression):
+        result = list()
+        position = 0
+        for item in expression.split(" "):
+            if item in self.OPERATORS:
+                if position % 2 != 0:   # es par
+                    result.append(item)
+                else:
+                    raise CalculatorSyntaxError("Unexpected operator {0} found".format(item))
+            else:
+                try:
+                    if position % 2 == 0:  # es par
+                        result.append(int(item))  # convierto item en un entero
+                    else:
+                        raise CalculatorSyntaxError("Unexpected number {0} found".format(item))
+                except ValueError:
+                    raise CalculatorSyntaxError("Unknown item {0}".format(item))
+            position += 1
+        return result
+
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from calculator import Calculator
+from calculator import Calculator, CalculatorSyntaxError
 
 
 class TestsCalculator(unittest.TestCase):
@@ -38,6 +38,42 @@ class TestsCalculator(unittest.TestCase):
         result_a = self.calculator.subtract(1, 6)
         result_b = self.calculator.subtract(6, 1)
         self.assertNotEqual(result_a, result_b)
+
+    def test_eval_add_4_6(self):
+        result = self.calculator.eval("4 + 6")
+        self.assertEqual(result, 10)
+
+    def test_parse_add_4_6(self):
+        result = self.calculator.parse("4 + 6")
+        self.assertEqual(result, [4, "+", 6])
+
+    def test_parse_add_6_6(self):
+        result = self.calculator.parse("6 + 6")
+        self.assertEqual(result, [6, "+", 6])
+
+    def test_parse_add_1_2_3(self):
+        result = self.calculator.parse("1 + 2 + 3")
+        self.assertEqual(result, [1, "+", 2, "+", 3])
+
+    def test_parse_subtract_1_6(self):
+        result = self.calculator.parse("1 - 6")
+        self.assertEqual(result, [1, "-", 6])
+
+    def test_bad_expression_syntax_with_two_operators_raises_exception(self):
+        with self.assertRaises(CalculatorSyntaxError):
+            self.calculator.parse("1 + + 5")
+
+    def test_bad_expression_syntax_with_unknown_item_raises_exception(self):
+        with self.assertRaises(CalculatorSyntaxError):
+            self.calculator.parse("1 + alberto")
+
+    def test_bad_expression_syntax_with_two_numbers_without_operator_between(self):
+        with self.assertRaises(CalculatorSyntaxError):
+            self.calculator.parse("1 + 2 3")
+
+    def test_bad_expression_syntax_with_unknown_operator(self):
+        with self.assertRaises(CalculatorSyntaxError):
+            self.calculator.parse("1 menos 2")
 
 
 # comprueba si se está ejecutando directamente este archivo con el comando: python tests.py y de ser así, arranca los test
